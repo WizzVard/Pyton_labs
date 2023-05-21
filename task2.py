f5b76c1589fa0b280c1cdd3a1995f2ee0c3ae2a8
WizzVard <ender22032004@gmail.com>
2023-05-22 02:34:00 +0300

    add task 2

---
+++ task2.py
@@ -0,0 +1,11 @@
+# Introduction to programming, Task 2, Vadim Kustov
+
+print("Introduction to programming: Task 2")
+print("Kustov Vadim")
+
+
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+z = int(input("Enter z: "))
+
+print("Result is", x / (y - 211.3) - (z + 11.3) / x)
